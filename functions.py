1c3dd0768bc6bde6f977dadd6f16bff0c640220a
lfrecalde1 <lfrecalde1@espe.edu.ec>
2021-12-08 13:34:36 -0500

    Pregunta 5

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
+import imutils
+from imutils.object_detection import non_max_suppression
 
 def data(path,aux=1):                                                                                                                                                                                                                      
     images = []                                                                                                         
@@ -359,3 +361,37 @@ def pregunta_4(imgs, path):
         cv2.waitKey(0)
         j = j + 1
     return None
+
+def pregunta_5(imgs, path):
+    img = imgs[0, :, :]
+    scale = 1.0
+    w = int(img.shape[1] / scale)
+    image = imutils.resize(img, width=w)
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    orig = img.copy()
+
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(img, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # apply non-maxima suppression to the bounding boxes using a
+        # fairly large overlap threshold to try to maintain overlapping
+        # boxes that are still people
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.90)
+
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick: cv2.rectangle(img, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    cv2.imshow("Output", orig)
+    cv2.imshow("OutputNMS", img)
+    cv2.waitKey(0)
+    name1 = "Pregunta_5_{}.png".format(0)
+    guardar(path, name1, orig)
+    return None
+
--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import os
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from functions import *
+import imutils
+from imutils.object_detection import non_max_suppression
 
 ## Direcciones de donde se va sacar las Imagenes
 path_o = '/home/fer/Control_servo_visual/Code/Practico_3.0/Pictures/'
 path_o1 = '/home/fer/Control_servo_visual/Code/Practico_3.0/Tools/'
 path_o2 = '/home/fer/Control_servo_visual/Code/Practico_3.0/Letters/'
 path_o3 = '/home/fer/Control_servo_visual/Code/Practico_3.0/Leafs/'
+path_o4 = '/home/fer/Control_servo_visual/Code/Practico_3.0/Persons/'
 
 path_w = '/home/fer/Control_servo_visual/Code/Practico_3.0/Modificadas/'
 
 
-
 def literal_1():
+    print("Pregunta 1")
     imgs = data(path_o, 1)
     ## Pregunta_1
     trans = np.array([[25, 0, 0], [25, 0, 0]])
@@ -25,37 +28,38 @@ def literal_1():
     scales = np.array([[1, 1, 0.3]])
 
     pregunta_1(imgs, trans, angles, scales, path_w)
-    return None
 
 def literal_2():
+    print("Pregunta 2")
     ## Pregunta 2
     imgs = data(path_o1, 1)
-
     Pregunta_2(imgs, path_w)
-    return None
 
 def literal_3():
+    print("Pregunta 3")
     ## Pregunta 3
     imgs = data(path_o2, 1)
     img = imgs[0, :, :]
     Pregunt_3(img, path_w)
-    return None
-
 
 
 def literal_4():
+    print("Pregunta 4")
     ## Pregunta 4
     imgs = data(path_o3, 1)
     pregunta_4(imgs, path_w)
     
-
+def literal_5():
+    print("Pregunta 5")
+    imgs = data(path_o4, 1)
+    pregunta_5(imgs, path_w)
 
 def main():
     #literal_1()
     #literal_2()
-    literal_3()
+    #literal_3()
     #literal_4()
-
+    literal_5()
 
 if __name__ == '__main__':
     try:
